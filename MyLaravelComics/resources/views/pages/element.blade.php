b7193e1fa544406844a50cc33d4847e2eb8b0db3
paolotrimini <paolo.trimini@gmail.com>
2021-05-18 17:57:16 +0200

    single page box talent - specs

--- MyLaravelComics/resources/views/pages/element.blade.php
+++ MyLaravelComics/resources/views/pages/element.blade.php
@@ -1,17 +1,18 @@
     @extends('layouts.main-layout')
     @section('content')
         <div class="single-page">
-            <div class="box-section">
+            {{-- COMIC --}}
+            <div class="box-blue">
             </div>
             <div class="container">
                 <div class="box-comic">
                     {{-- INFO --}}
-                    <div class="box-info">
+                    <div class="box-info-1">
                         <div class="box-title">
                             <h1>{{ $elem['title'] }}</h1>
                         </div>
                         {{-- HEADER BOX COMIC --}}
-                        <div class="box-line">
+                        <div class="box-green">
                             <div class="box-price">
                                 <p>U.S. Price: {{ $elem['price'] }}</p>
                                 <p>Available</p>
@@ -30,6 +31,17 @@
                         <div class="box-img">
                             <img src="{{ asset('/storage/assets/adv.jpg') }}" alt="adv">
                         </div>
+                    </div>
+                </div>
+            </div>
+            {{-- INFO--}}
+            <div class="box-info-2">
+                <div class="container">
+                    <div class="box-talent">
+                        <p>talent</p>
+                    </div>
+                    <div class="box-specs">
+                        <p>specs</p>
                     </div>
                 </div>
             </div>
