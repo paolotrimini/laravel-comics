d3d9fa0a3adb0a0e351580321692adbdd5d23c85
paolotrimini <paolo.trimini@gmail.com>
2021-05-18 15:54:33 +0200

    single page

--- MyLaravelComics/resources/views/pages/element.blade.php
+++ MyLaravelComics/resources/views/pages/element.blade.php
@@ -1,10 +1,19 @@
     @extends('layouts.main-layout')
     @section('content')
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h2>{{ $elem['title'] }}</h2>
-                    <p>{{ $elem['description'] }}</p>
+        <div class="single-page">
+            <div class="line-section">
+            </div>
+            <div class="container">
+                <div class="box-comic">
+                    <div class="box-title">
+                        <h1>{{ $elem['title'] }}</h1>
+                    </div>
+                    <div class="box-info">
+                        <p>U.S. Price: {{ $elem['price'] }}</p>
+                    </div>
+                    <div class="box-description">
+                        <p>{{ $elem['description'] }}</p>
+                    </div>
                 </div>
             </div>
         </div>
